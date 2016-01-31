fc2de25e1ae13bb4ee41926ddcdec0acc4e5f8e2
Group 36 <leejiaen1212@gmail.com>
2016-01-31 16:28:07 +0800

    Create relayon.py

---
+++ relayon.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# Group 36 (Ng Chee Ming and Lee Jia En)
+# Trigger from an AWS rule with the actuating of buzzer.
+
+import RPi.GPIO as GPIO  
+from time import time  
+  
+#GPIO.setmode(GPIO.BOARD)  
+#GPIO.setwarnings(False)
+#number = 40
+#GPIO.setup(number,GPIO.OUT)  
+#GPIO.output(number,True)
+
+
+GPIO.setmode(GPIO.BCM)  
+number = 21  
+GPIO.setup(number, GPIO.OUT)  
+GPIO.output(number, GPIO.HIGH) 
